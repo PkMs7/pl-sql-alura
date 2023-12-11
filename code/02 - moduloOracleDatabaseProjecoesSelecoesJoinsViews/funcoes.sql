a11f56ece61b841cfa8a316aa42a226a49aed46d
PkMs7 <patrick.marques@zaffarinet.com.br>
2023-12-11 12:00:00 -0300

    Ajuste nas anotações

---
+++ code/02 - moduloOracleDatabaseProjecoesSelecoesJoinsViews/funcoes.sql
@@ -0,0 +1,23 @@
+-- Funções de String
+
+LOWER() -- Transforma todos os textos em letras minúsculas
+UPPER() -- Transforma todos os textos em letras maiúsculas
+INITCAP() -- Transforma a primeira letra de cada palavra em maiúscula
+CONCAT() -- Concatena todas as informações
+LPAD() -- Acrescenta caracteres na frente do texto
+RPAD() --Acrescenta caracteres no final do texto
+SUBSTR() -- Retira trechos de um texto maior
+INSTR() -- Testa se uma determinada cadeia de caracteres está contida no texto
+LTRIM() -- Retira espaços à esquerda do caractere
+RTRIM() -- Retira espaços à direita do caractere
+TRIM() -- Retira espaços à direita e à esquerda do campo
+TRANSLATE() -- Traduz determinados caracteres de um nome para outros caracteres
+REPLACE() -- Traduz uma cadeia de caracteres de um nome para outros caracteres
+
+-- Funções de Data
+
+
+-- Funções de Números
+
+
+-- Funções de Conversão
